3c489c30e40b7debbb787e143da4444dc208c061
Iasi Salomon <iasisalomon@gmail.com>
2020-09-14 00:48:35 -0300

    final con poco estilo

--- iasisalomon2/src/App.vue
+++ iasisalomon2/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div class="container">
-      <div  id="nav" class="nav nav-tabs nav-justified">
+      <div class="nav nav-pills nav-justified">
         <router-link class="nav-item" to="/">Bitcoin Converter</router-link>
         <router-link class="nav-item" to="candles">Candlesticks Graph</router-link>
       </div>
--- iasisalomon2/src/views/Candles.vue
+++ iasisalomon2/src/views/Candles.vue
@@ -1,21 +1,18 @@
 <template>
   <div class="graph">
     <div class="container">
-      <div class="row">
-        <appCandle></appCandle>
+      <div class="row justify-content-center">
+        <appCandle class="col-12" ></appCandle>
     </div>
   </div>
 </div>
 </template>
 
 <script>
-
 import appCandle from '../components/appCandle'
 
-// @ is an alias to /src
-
 export default {
-  name: "Home",
+  name: "Candle",
   components: {
     appCandle,
   }
