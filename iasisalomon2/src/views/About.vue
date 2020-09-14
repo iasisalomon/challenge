a2362333dc2736fe3f287fee23cb1ffcc3e70565
Iasi Salomon <iasisalomon@gmail.com>
2020-09-14 00:15:06 -0300

    candles

--- iasisalomon2/src/views/About.vue
+++ iasisalomon2/src/views/About.vue
@@ -1,5 +1,23 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="graph">
+    <div class="container">
+      <div class="row">
+        <appCandle></appCandle>
+    </div>
   </div>
+</div>
 </template>
+
+<script>
+
+import appCandle from '../components/appCandle'
+
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  components: {
+    appCandle,
+  }
+};
+</script>
